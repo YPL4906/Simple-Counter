4fa43b6223bd605152b6aa159e6dfbbd6d1d21b2
YPL4906 <104123424+YPL4906@users.noreply.github.com>
2024-12-19 13:15:46 +0000

    SimpleCounter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = ({numero1, numero2, numero3, numero4}) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container">
+            <div className="calendar">
+			<i class="bi bi-stopwatch"></i>
+            </div>
+            <div className="digitoCuatro">{numero4}</div>
+            <div className="digitoTres">{numero3}</div>
+            <div className="digitoDos">{numero2}</div>
+            <div className="digitoUno">{numero1}</div>
+        </div>
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,29 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+let num1 = 0
+let num2 = 0
+let num3 = 0
+let num4 =0
+
+setInterval(function(){
+    num1++;
+    if (num1 === 10) {
+      num1 = 0;
+      num2++;
+      if (num2 === 10) {
+        num2 = 0;
+        num3++;
+        if (num3 === 10) {
+          num3 = 0;
+          num4++;
+          if (num4 === 10) {
+            num4 = 0;
+          }
+        }
+      }
+    }
+        ReactDOM.createRoot(document.getElementById('app')).render(<Home numero1={num1} numero2={num2} numero3={num3} numero4={num4}/>);
+}, 1000);
+
 
